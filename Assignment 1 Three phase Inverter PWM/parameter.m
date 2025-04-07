fcab225bbdbb6f084eccbd8b4ce82b4d5c5d6344
yadukrishnan-k-m <ee24mt013@iitdh.ac.in>
2025-04-07 14:47:17 +0530

    Add files via upload
    Simulation assignment 1 and 4

---
+++ Assignment 1 Three phase Inverter PWM/parameter.m
@@ -0,0 +1,10 @@
+% Parameter file for Simulink model
+
+% Inverter parameters
+Vdc = 200;          % Volts
+L = 0.08;           % H
+Rl = 80;            % Ohms
+
+% Signal parameters
+Fs = 10e3;          % Hz
+Fsig = 314.16;      % Radians
